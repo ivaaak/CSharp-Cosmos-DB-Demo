9f60aca709399bc480492a086ee90e0d343c257d
Ivo <43663336+ivaaak@users.noreply.github.com>
2022-05-05 12:01:13 +0300

    initial commit

---
+++ CosmosService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+using System;
+using System.Threading.Tasks;
+
+using static CosmosDemo.CRUD.Create;
+using static CosmosDemo.CRUD.Read;
+using static CosmosDemo.CRUD.Update;
+using static CosmosDemo.CRUD.Delete;
+
+using static CosmosDemo.Data.Constants;
+
+namespace CosmosDemo;
+
+public class CosmosService
+{
+    public async Task ExecuteCommand(string command)
+    {
+        using (var cosmosClient = new CosmosClientBuilder(EndpointUri, PrimaryKey).WithApplicationRegion(Regions.WestEurope).Build())
+        {
+            switch (command)
+            {
+                case "1":
+                    await CreateDatabaseAsync(cosmosClient);
+                    break;
+                case "2":
+                    await CreateContainerAsync(cosmosClient);
+                    break;
+                case "3":
+                    await ScaleContainerAsync(cosmosClient);
+                    break;
+                case "4":
+                    await AddItemsAsync(cosmosClient);
+                    break;
+                case "5":
+                    await QueryItemsAsync(cosmosClient);
+                    break;
+                case "6":
+                    await UpdateItemAsync(cosmosClient);
+                    break;
+                case "7":
+                    await DeleteItemAsync(cosmosClient);
+                    break;
+                case "8":
+                    await ConcurrencyUpdateAsync(cosmosClient);
+                    break;
+                case "9":
+                    await ExecuteStoredProcedureAsync(cosmosClient);
+                    break;
+                case "10":
+                    await DeleteDatabaseAsync(cosmosClient);
+                    break;
+                default:
+                    Console.WriteLine($"Invalid command: {command}");
+                    break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using CosmosDemo;
+using Microsoft.Azure.Cosmos;
+using System;
+
+CosmosService cosmosService = new();
+
+while (true)
+{
+    Console.WriteLine("1 - Create Database");
+    Console.WriteLine("2 - Create Container");
+    Console.WriteLine("3 - Scale Container");
+    Console.WriteLine("4 - Add Items");
+    Console.WriteLine("5 - Query Items");
+    Console.WriteLine("6 - Update Item");
+    Console.WriteLine("7 - Delete Item");
+    Console.WriteLine("8 - Concurrency Update");
+    Console.WriteLine("9 - Execute Stored Procedure");
+    Console.WriteLine("10 - Delete Database");
+    Console.WriteLine();
+    Console.Write("Press command number or 'e' for exit: ");
+
+    string input = Console.ReadLine();
+    Console.Clear();
+
+    if (input is "e") break;
+
+    try
+    {
+        await cosmosService.ExecuteCommand(input);
+    }
+    catch (CosmosException de)
+    {
+        Exception baseException = de.GetBaseException();
+        Console.WriteLine("{0} error occurred: {1}\n", de.StatusCode, de);
+        Console.WriteLine("Press any key to continue..");
+        Console.ReadKey();
+        Console.Clear();
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Error: {0}\n", e);
+        Console.WriteLine("Press any key to continue..");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
+
+Console.WriteLine("End of demo.");
